6c902248c0df214cb4daa0ee892ff50c1faec12a
KarineGeorge <1900499@eng.asu.edu.eg>
2022-05-16 10:55:23 +0200

    keypad

--- Controller.c
+++ Controller.c
@@ -1,9 +1,24 @@
 #include "tm4c123gh6pm.h"
-#include "led.c"
+#include "led.h"
+#include "lcd.h"
+#include "delay.h"
+#include "keypad.h"
 
-int main(){
+//Testing keypad
+int main(){               
 	RGBLED_Init();
+	LCD_Init();
+	keypad_Init();
 	
+	RGB_ON();
+	while(1){
+		while(keypad_getkey()==0xFF);
+		unsigned char c = keypad_getkey();
+		LCD_char(c);
+		delay_s(2);
+		LCD_VCLRScreen();
+	
+	}
 	
 	
 }
--- controller.d
+++ controller.d
@@ -1,4 +1,9 @@
 .\controller.o: Controller.c
 .\controller.o: tm4c123gh6pm.h
-.\controller.o: led.c
-.\controller.o: led.h 
+.\controller.o: led.h
+.\controller.o: lcd.h
+.\controller.o: delay.h
+.\controller.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\controller.o: delay.h
+.\controller.o: keypad.h
+.\controller.o: delay.h
