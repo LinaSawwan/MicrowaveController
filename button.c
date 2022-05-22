d120c60c6b805fc42feb3fb95857f0f76472bff1
unknown <1900242@eng.asu.edu.eg>
2022-05-23 00:16:44 +0200

    editted button driver

--- button.c
+++ button.c
@@ -1,6 +1,6 @@
 #include "button.h"
 
-void SWs_Init(){                        //sw1&2 in F4,F0 and sw3 in A2
+void SWs_Init(){                        //sw1&2 in F4,F0 and sw3 in A1
 SYSCTL_RCGCGPIO_R |= 0x21;
 while ((SYSCTL_PRGPIO_R & 0x21)==0);
 GPIO_PORTF_LOCK_R = 0x4C4F434B;
@@ -28,15 +28,18 @@ GPIO_PORTF_DEN_R |= 0x11;
 GPIO_PORTA_DEN_R |= 0x04;
 }
 
+
 unsigned char SW2_Read(){
   return ((GPIO_PORTF_DATA_R & (1<<0))>>0);
-}	
+}
 
 unsigned char SW1_Read(){
-	return ((GPIO_PORTF_DATA_R & (1<<4))>>4);
+	int n = ((GPIO_PORTF_DATA_R & (1<<4))>>4);
+	return n;
 }
 
 unsigned char SW3_Read(){
-	return ((GPIO_PORTA_DATA_R & (1<<2))>>2);
+	int n = ((GPIO_PORTA_DATA_R & (1<<2))>>2);
+	return n;
 }
 
--- button.h
+++ button.h
@@ -1,4 +1,5 @@
 #include "tm4c123gh6pm.h"
+#define READ_BIT(reg,bit) (reg & (1<<bit))>>bit
 
 void SWs_Init(void);
 unsigned char SW1_Read(void);
